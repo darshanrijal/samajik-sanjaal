94d6e28d12051c91d42d5d2611f205e6c3164ea7
Darshan rijal <darshanrijal0@gmail.com>
2024-09-08 17:43:09 +0545

    create trending topics sidebar also who to follow section

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,6 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
 import Post from "@/components/posts/Post";
+import TrendsSidebar from "@/components/TrendsSidebar";
 import prisma from "@/lib/prisma";
 import { PostDataInclude } from "@/lib/types";
 
@@ -11,13 +12,14 @@ export default async function Home() {
     include: PostDataInclude,
   });
   return (
-    <main className="w-full min-w-0">
+    <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
         {posts.map((post) => (
           <Post post={post} key={post.id} />
         ))}
       </div>
+      <TrendsSidebar />
     </main>
   );
 }
