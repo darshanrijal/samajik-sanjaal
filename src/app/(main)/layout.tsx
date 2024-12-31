d06b38683f998df8267b7cb3291e449e22194b9e
Darshan rijal <darshanrijal0@gmail.com>
2024-12-31 15:12:07 +0545

    add MenuBar component and integrate it into MainLayout; enhance layout structure for better responsiveness

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 import { TRPCReactProvider } from "@/__rpc/react";
 import { getCurrentSession } from "@/auth";
 import { redirect } from "next/navigation";
+import { MenuBar } from "./mebubar";
 import { Navbar } from "./navbar";
 import { SessionProvider } from "./session-provider";
 
@@ -16,7 +17,11 @@ export default async function MainLayout({
       <TRPCReactProvider>
         <div className="flex min-h-screen flex-col">
           <Navbar />
-          <div className="mx-auto max-w-7xl p-5">{children}</div>
+          <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
+            <MenuBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
+            {children}
+          </div>
+          <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
         </div>
       </TRPCReactProvider>
     </SessionProvider>
